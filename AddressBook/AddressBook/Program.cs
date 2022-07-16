399e4a0c5a6f9c528e6ab810bea42e0f226deff9
Sourav2707 <smartsourav33@gmail.com>
2022-07-16 23:08:55 +0530

    [Sourav] - Creating contacts

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -9,5 +9,4 @@ Console.WriteLine("Welcome to Address Book Program in Address Book Main class on
 Console.WriteLine("Enter the details to create a contact");
 Address obj = new Address();
 obj.create();
-obj.create();
 obj.display();
